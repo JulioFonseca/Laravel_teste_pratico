f6d27a2c47b1852ce594d6c343d7bcb276112129
Julio Fonseca <juliocezar5716@gmail.com>
2021-03-14 18:57:20 -0300

    Criei o banco de dados e fiz o controller

---
+++ database/migrations/2021_03_14_213912_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('cpf')->unique();
+            $table->string('telefone');
+            $table->string('password');
+            $table->date('data');
+            $table->rememberToken();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
